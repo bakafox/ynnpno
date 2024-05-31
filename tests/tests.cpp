8b36e3000023e47dcf27602f1a6a29a3a664a984
bakafox <v.chekhovskii@g.nsu.ru>
2024-05-31 11:26:37 +0700

    Новая версия программы из task1

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,83 +1,76 @@
+#include <gtest/gtest.h>
 #include "../include/bitset.h"
-#include <assert.h>
 
 using namespace std;
 
 
 
-void doTests() {
-	BitArray test1;
-	assert(test1.empty());
+TEST(bitset_test, my_test) {
+    BitArray test1;
+    ASSERT_TRUE(test1.empty());
 
-	BitArray test2(5, true);
-	assert(!test2.empty());
-	assert(test2.size() == 5);
-	assert(test2.to_string() == "11111");
+    BitArray test2(5, true);
+    ASSERT_FALSE(test2.empty());
+    ASSERT_EQ(test2.size(), 5);
+    ASSERT_EQ(test2.to_string(), "11111");
 
-	BitArray test3(test2);
-	assert(test3 == test2);
+    BitArray test3(test2);
+    ASSERT_EQ(test3, test2);
 
-	test3.resize(8, false);
-	assert(test3.size() == 8);
-	assert(test3.to_string() == "11111000");
-	test3.set(5, true);
-	assert(test3.to_string() == "11111100");
-	test3.reset(4);
-	test3.reset(5);
-	assert(test3.to_string() == "11110000");
+    test3.resize(8, false);
+    ASSERT_EQ(test3.size(), 8);
+    ASSERT_EQ(test3.to_string(), "11111000");
+    test3.set(5, true);
+    ASSERT_EQ(test3.to_string(), "11111100");
+    test3.reset(4);
+    test3.reset(5);
+    ASSERT_EQ(test3.to_string(), "11110000");
 
-	test3.resize(5);
-	assert((test2 & test3).to_string() == "11110");
-	assert((test2 | test3).to_string() == "11111");
-	assert((test2 ^ test3).to_string() == "00001");
-	assert((~test3).to_string() == "00001");
+    test3.resize(5);
+    ASSERT_EQ((test2 & test3).to_string(), "11110");
+    ASSERT_EQ((test2 | test3).to_string(), "11111");
+    ASSERT_EQ((test2 ^ test3).to_string(), "00001");
+    ASSERT_EQ((~test3).to_string(), "00001");
 
-	assert((test3 << 3).to_string() == "11000");
-	assert((test3 >> 2).to_string() == "00111");
+    ASSERT_EQ((test3 << 3).to_string(), "11000");
+    ASSERT_EQ((test3 >> 2).to_string(), "00111");
 
-	assert(test2 == test2);
-	assert(test2 != test3);
+    ASSERT_EQ(test2, test2);
+    ASSERT_NE(test2, test3);
 
-	BitArray test4(29);
-	test4.set();
-	assert(test4.any() == true);
-	assert(test4.none() == false);
-	test4.reset();
-	assert(test4.any() == false);
-	assert(test4.none() == true);
+    BitArray test4(29);
+    test4.set();
+    ASSERT_TRUE(test4.any());
+    ASSERT_FALSE(test4.none());
+    test4.reset();
+    ASSERT_FALSE(test4.any());
+    ASSERT_TRUE(test4.none());
 
-	test4.set(2);
-	test4.set(3);
-	test4.set(6);
-	test4.set(8);
-	assert(test3.count() == 4);
+    test4.set(2);
+    test4.set(3);
+    test4.set(6);
+    test4.set(8);
+    ASSERT_EQ(test3.count(), 4);
 
-	test1.push_back(true);
-	assert(test1.size() == 1);
-	assert(test1.to_string() == "1");
+    test1.push_back(true);
+    ASSERT_EQ(test1.size(), 1);
+    ASSERT_EQ(test1.to_string(), "1");
 
-	test1.clear();
-	assert(test1.size() == 0);
+    test1.clear();
+    ASSERT_EQ(test1.size(), 0);
 
-	BitArray test5(3, true);
-	test5.resize(100);
-	test5.set();
-	assert(test5.to_string() == "1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111");
-	test5.reset();
-	assert(test5.to_string() == "0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000");
-	BitArray test6(100, true);
-	for (int t = 0; t < 100; t+=3) {
-		test6.set(t, false);
-		test5.set(t/2, true);
-	}
-	assert((test6 & test5).to_string() == "0100100100100100100100100100100100100100100100100100000000000000000000000000000000000000000000000000");
-	assert((test6 | test5).to_string() == "1111111111111111111111111111111111111111111111111110110110110110110110110110110110110110110110110110");
-	assert((test6 ^ test5).to_string() == "1011011011011011011011011011011011011011011011011010110110110110110110110110110110110110110110110110");
-}
-
-
-int main() {
-    doTests();
-
-	return 0; // All tests completed successfully!
+    BitArray test5(3, true);
+    test5.resize(100);
+    test5.set();
+    ASSERT_EQ(test5.to_string(), "1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111");
+    test5.reset();
+    ASSERT_EQ(test5.to_string(), "0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000");
+    BitArray test6(100, true);
+    for (int t = 0; t < 100; t += 3) {
+        test6.set(t, false);
+        test5.set(t / 2, true);
+    }
+    ASSERT_EQ((test6 & test5).to_string(), "0100100100100100100100100100100100100100100100100100000000000000000000000000000000000000000000000000");
+    ASSERT_EQ((test6 | test5).to_string(), "1111111111111111111111111111111111111111111111111110110110110110110110110110110110110110110110110110");
+    ASSERT_EQ((test6 ^ test5).to_string(), "1011011011011011011011011011011011011011011011011010110110110110110110110110110110110110110110110110");
 }
